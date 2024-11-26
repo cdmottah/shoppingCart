a069e20464734d66fc8121ac00a0de1e313f4d41
Cristian Danilo Motta Herrera <cdmottah@gmail.com>
2024-11-25 21:43:05 -0500

    feat(header, product): enhance cart display and functionality
    Enhanced the cart's UI and added a computed property to calculate the total price.
    
    **Header Component:**
    - Improved cart display structure by adding distinct sections (header, body, footer) in the HTML.
    - Enhanced cart styling with SCSS updates for better readability and maintainability.
    - Introduced `computed` property to calculate the total cart price dynamically.
    - Fixed minor formatting issues in TypeScript imports.
    
    **Product Component:**
    - Removed unnecessary blank lines from the HTML for cleaner structure.

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, signal, WritableSignal } from '@angular/core';
+import { Component, Input, signal, WritableSignal, computed } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 import { faCartShopping, faClose } from '@fortawesome/free-solid-svg-icons';
 import { Product } from '../../models/product.model';
@@ -7,7 +7,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule,FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
@@ -16,8 +16,13 @@ export class HeaderComponent {
   readonly faClose = faClose
 
   hideSideMenu = signal(true)
-  @Input({required:true}) cart!: WritableSignal<Product[]>
 
+  @Input({ required: true }) cart!: WritableSignal<Product[]>
+
+  total = computed(() => {
+    const cartValue = this.cart();
+    return cartValue.reduce((prevValue, currentValue) => { return prevValue += currentValue.price }, 0)
+  })
   toggleSideMenu() {
     this.hideSideMenu.update(res => !res)
   }
